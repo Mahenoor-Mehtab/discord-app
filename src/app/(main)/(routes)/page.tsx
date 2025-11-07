25af458f3019852705cb9c528509b03ef03af739
Mahenoor Mehtab <mahenoormehtab31@gmail.com>
2025-11-07 11:15:55 +0530

    03_theme-setup

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,11 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
     <div>
-      this is a protected routes
+     <h1> this is a protected routes</h1>
+
+        <ModeToggle/>
     </div>
+  
     </>
 
   );
