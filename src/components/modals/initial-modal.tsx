2810b5bb9ad903c929a79d84b3629ba7826ad101
Mahenoor Mehtab <mahenoormehtab31@gmail.com>
2025-11-08 20:19:28 +0530

    05_update_home_page

---
+++ src/components/modals/initial-modal.tsx
@@ -0,0 +1,43 @@
+'use client'
+import {useForm} from 'react-hook-form'
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+const InitialModal = () => {
+    const form = useForm({
+        defaultValues:{
+            name:"",
+            imageUrl:"",
+        }
+    })
+    return ( 
+        <>
+        <Dialog open>
+          <DialogContent className="bg-white text-black p-0 overflow-hidden">
+
+        <DialogHeader className="pt-8 px-6">
+        <DialogTitle className="text-2xl text-center font-bold">
+        Customize your server
+        </DialogTitle>
+      <DialogDescription className="text-center text-zinc-500">
+Give your server a personality with a name and an image. You can alaways change it later.
+      </ DialogDescription>
+        </DialogHeader>
+        </DialogContent>
+
+            </Dialog>
+        </>
+     );
+}
+ 
+export default InitialModal;
---
+++ src/lib/initial-profile.ts
@@ -0,0 +1,29 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/db";
+export const initialProfile = async () =>{
+    const user = await currentUser();
+
+    if (!user) {
+    return redirect("/sign-in");
+  }
+
+    const  profile = await prisma.profile.findUnique({
+        where:{
+            userId :user.id
+
+        }
+    })
+    if(profile) return profile;
+
+    const newProfile = await prisma.profile.create({
+        data:{
+            userId:user.id,
+            name:`${user.firstName} ${user.lastName}`,
+            imageUrl:user.imageUrl,
+            email:user.emailAddresses[0].emailAddress
+        }
+
+    })
+ return newProfile;
+}
